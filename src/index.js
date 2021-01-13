c8dab10224228135b429b11b673a00869673bc18
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-13 19:25:55 +1030

    JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 // 1.) import the above
 
 // 2.) setup a function (called component in react).
 // Function must begin with a capital letter for react to recognise it
-// stateless functional component, always return JSX 
+// stateless functional component, always return JSX
 function Greeting() {
-  return <div>
-    <h1>Hello World</h1>
-  </div>
+  // JSX Rules
+  // returns only single element
+  // div / section / article or fragment
+  // use camelCase for html attribute
+  // className, not class
+  // close every element + format! (generally prettier takes care of it)
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ul>
+        <li>
+          <a href="#">Hello World</a>
+        </li>
+        <img src="" alt="" />
+        <input type="text" name="" id="" />
+      </ul>
+    </div>
+  );
 }
+
+// 3.) import react-dom will allow us to inject the function into the 'root' element in the index.html
+// we call .render and pass the function and document.getElementById('root')
+ReactDom.render(<Greeting />, document.getElementById("root"));
 
 // basically the same code as above but what it is actually doing under-the-hood
 // const Greeting = () => {
@@ -19,8 +38,3 @@ function Greeting() {
 //     React.createElement('h1', {}, 'hello world')
 //   );
 // };
-
-
-// 3.) import react-dom will allow us to inject the function into the 'root' element in the index.html 
-// we call .render and pass the function and document.getElementById('root')
-ReactDom.render(<Greeting />, document.getElementById('root'));
