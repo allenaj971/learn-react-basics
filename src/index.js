045746da4661f54793cff3d1eb13b8ef447dd56f
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-13 19:54:44 +1030

    mini book project

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import ReactDom from "react-dom";
 // Generally, we have a main entry function (naming convention is App but here
 // we are calling it Greeting), then we include the components we write below it or in
 // other files and call it using JSX.
-function Greeting() {
+function Booklist() {
   // JSX Rules
   // returns only single element
   // div / section / article or fragment
@@ -18,40 +18,30 @@ function Greeting() {
   // className, not class
   // close every element + format! (generally prettier takes care of it)
   return (
-    <div>
-      <h1>Hello World</h1>
-      <ul>
-        <li>
-          <a href="#">Hello World</a>
-        </li>
-        <img src="" alt="" />
-        <input type="text" name="" id="" />
-      </ul>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+    </section>
   );
 }
-// Component Person
-const Person = () => {
+
+const Book = () => {
   return (
-    <div>
-      <h2>John Doe</h2>
-    </div>
-  );
-};
-// Component Message
-const Message = () => {
-  return (
-    <div>
-      <p>this is my message</p>
-    </div>
+    <article>
+      <Image />
+      <Title />
+    </article>
   );
 };
 
+const Image = () => (
+  <img src="https://tesla-cdn.thron.com/delivery/public/image/tesla/35d15221-0a5f-4dce-b484-a4db67b81dd2/bvlatuR/std/0x0/model-s@2x" />
+);
+
+const Title = () => <h1>Tesla Model S</h1>;
+
 // 3.) import react-dom will allow us to inject the function into the 'root' element in the index.html
 // we call .render and pass the function and document.getElementById('root')
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<Booklist />, document.getElementById("root"));
 
 // basically the same code as above but what it is actually doing under-the-hood
 // const Greeting = () => {
