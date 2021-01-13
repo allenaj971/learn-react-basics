ecfdffca6af5bd671a56140fbffb1a74119146e5
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-13 20:55:15 +1030

    jsx javascript

--- src/index.js
+++ src/index.js
@@ -24,33 +24,30 @@ function Booklist() {
     // add classname to elements so that they may be referenced in the index.css file
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
-
+// adding styling through jsx. jsx css or imported libraries overwrites any styling applied
+// in index.css
 const Book = () => {
+  const title = "Gutenberg";
+  // referencing variables inside html, use {'insert_variable_here'}
+
   return (
     <article className="book">
-      <Image />
-      <Title />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg/447px-Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg" />
+      <h1
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      >
+        {title.toLocaleUpperCase()}
+      </h1>
+      {/* <p>{let x = 6}</p> */}
+      {/* The above doesn't work, JSX requires that it return a value, which the above
+      does not  */}
+      <p>{6 + 6}</p>
     </article>
   );
 };
-
-const Image = () => (
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg/447px-Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg" />
-);
-// adding styling through jsx.
-const Title = () => (
-  <h1 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Some random book
-  </h1>
-);
 
 // 3.) import react-dom will allow us to inject the function into the 'root' element in the index.html
 // we call .render and pass the function and document.getElementById('root')
