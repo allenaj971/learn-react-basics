46c64ca8b3d8184d5ba3e52b5415897abf3f018a
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-13 19:44:10 +1030

    nested components and react browser developer tools

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import ReactDom from "react-dom";
 // 2.) setup a function (called component in react).
 // Function must begin with a capital letter for react to recognise it
 // stateless functional component, always return JSX
+
+// Nested Components, React Tools
+// Generally, we have a main entry function (naming convention is App but here
+// we are calling it Greeting), then we include the components we write below it or in
+// other files and call it using JSX.
 function Greeting() {
   // JSX Rules
   // returns only single element
@@ -22,9 +27,27 @@ function Greeting() {
         <img src="" alt="" />
         <input type="text" name="" id="" />
       </ul>
+      <Person />
+      <Message />
     </div>
   );
 }
+// Component Person
+const Person = () => {
+  return (
+    <div>
+      <h2>John Doe</h2>
+    </div>
+  );
+};
+// Component Message
+const Message = () => {
+  return (
+    <div>
+      <p>this is my message</p>
+    </div>
+  );
+};
 
 // 3.) import react-dom will allow us to inject the function into the 'root' element in the index.html
 // we call .render and pass the function and document.getElementById('root')
