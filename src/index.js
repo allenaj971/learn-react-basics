f0ec33fca15523ca981202dd2c6f93d5bdd8e900
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-13 19:04:07 +1030

    first component in detail

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import ReactDom from 'react-dom';
 
 // 2.) setup a function (called component in react).
 // Function must begin with a capital letter for react to recognise it
+// stateless functional component, always return JSX 
 function Greeting() {
-  return <h1>This is Allen and this is my first component</h1>;
+  return <div>
+    <h1>Hello World</h1>
+  </div>
 }
+
+// basically the same code as above but what it is actually doing under-the-hood
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'hello world')
+//   );
+// };
+
 
 // 3.) import react-dom will allow us to inject the function into the 'root' element in the index.html 
 // we call .render and pass the function and document.getElementById('root')
